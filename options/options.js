83e981572b823af180011ec5a07478dbf419e359
Crowdbotics <team@crowdbotics.com>
2022-03-27 16:51:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sims_fantasy_34458",
+  url: "https://sims_fantasy_34458.botics.co",
+  api: "https://sims_fantasy_34458.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
